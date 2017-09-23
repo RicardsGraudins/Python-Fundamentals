d7eb9adff5ab9959abb0bf14384a5401dcda8723
Ricards <G00314715@gmit.ie>
2017-09-23 16:45:20 +0100

    3. 01_hello_world.py

---
+++ Solutions/01_hello_world.py
@@ -0,0 +1,6 @@
+#The print() function simply outputs to console
+print("Hello World")
+
+#In Python every line of code is considered a statement of execution thus there is no need for semi-colons
+#at the end of statements. However semi colons can be used to delimit statements if you wish to put multiple
+#statements on the same line.
